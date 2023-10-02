936f5b1098efa09beebbb671226cce868a54f0fd
jamesarrojo.01 <jamesarrojo.01@gmail.com>
2023-10-02 17:16:33 +0800

    added fail message if user didn't get the word in 6 tries

--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -15,6 +15,18 @@ function InputText({ input, onInput, onSubmitGuess, guessHistory, answer }) {
                 </p>
             </div>
         );
+
+    if (
+        guessHistory[guessHistory.length - 1]?.['guess'] !== answer &&
+        guessHistory.length === 6
+    )
+        return (
+            <div className="sad banner">
+                <p>
+                    Sorry, the correct answer is <strong>{answer}</strong>.
+                </p>
+            </div>
+        );
     return (
         <form className="guess-input-wrapper" onSubmit={onSubmitGuess}>
             <label htmlFor="guess-input">Enter guess:</label>
